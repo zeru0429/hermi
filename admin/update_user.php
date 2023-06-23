a3a38cdfb965689182fc39e45e34300f2b188a7a
zeru0429 <mihiretutigistu@gmail.com>
2023-06-23 11:28:52 +0300

    update user

--- admin/update_user.php
+++ admin/update_user.php
@@ -1,89 +1,138 @@
-<?php include("./parts/header.php") ?>
+<?php include("./parts/header.php");
+$id = $_GET['id'];
 
-
+?>
 
  <!--USER MODAL-->
- 
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header bg-warning text-white">
-            <h5 class="modal-title">Update User</h5>
-            <button class="close" data-dismiss="modal">
-              <span>&times;</span>
-            </button>
-        </div>
 
-        <form action='./add-user.php' method='post' enctype='multipart/form-data' >
-          <div class="modal-body">
-           
-              <div class="form-group">
-                <label for="name">First name</label>
-                <input type="text" class="form-control" name='f_name' />
-              </div>
-
-              <div class="form-group">
-                <label for="name">middle name</label>
-                <input type="text"  name='m_name' class="form-control" />
-              </div>
-
-              <div class="form-group">
-                <label for="name">Last name</label>
-                <input type="text"  name='l_name' class="form-control" />
-              </div>
-
-              <div class="form-group">
-                <label for="name">username</label>
-                <input type="text"  name='username' class="form-control" />
-              </div>
-              <div class="form-group">
-                <label for="name">role</label>
-                <input type="text"  name='role' class="form-control" />
-              </div>
-
-              <div class="form-group">
-                <label for="name">Phone number</label>
-                <input type="text"  name='phone_number' class="form-control" />
-              </div>
-
-              <div class="form-group">
-                <label for="name">photo</label>
-                <td> <input type="file" name="image"> </td>
-              </div>
-
-              <div class="form-group">
-                <label for="Email">Email</label>
-                <input type="email" name='email' class="form-control" />
-              </div>
-
-              <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" name='password' class="form-control" />
-              </div>
-
-              <div class="form-group">
-                <label for="confirmPassword">Confirm Password</label>
-                <input type="password" name='c_password' class="form-control" />
-              </div>
-
-          
-          
-          <div class="modal-footer">
-            <button  data-dismiss="modal">Close</button>
-            <input type="submit"  class="btn btn-warning" name='submit' value="submit">
-          </div>
-
-          </div>
-        </form>
-        </div>
-      </div>
+<div class="modal-dialog modal-lg">
+<div class="modal-content">
+    <div class="modal-header bg-warning text-white">
+        <h5 class="modal-title">Update User</h5>
+        <button class="close" data-dismiss="modal">
+        <span>&times;</span>
+        </button>
     </div>
-    <!--  Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+        <?php
+                $query = "SELECT * FROM cbtp.users where user_id='$id'";
+                $result = mysqli_query($conn,$query) or die(mysqli_error());
+                $rows = mysqli_num_rows($result);  
+                if ($rows>0){
+                        while($rows=mysqli_fetch_assoc($result)){
+                        $username = $rows['username'];
+                        $rol = $rows['role'];
+                        $f_name = $rows['f_name'];
+                        $m_name = $rows["m_name"];
+                        $l_name=$rows['l_name'];
+                        $phone_number = $rows["phone_number"];
+                        $email = $rows['email']; 
+                       
+        ?>
 
 
+    <form action='#' method='post' enctype='multipart/form-data' >
+    <div class="modal-body">
+    
+        <div class="form-group">
+            <label for="name">First name</label>
+            <input type="text" class="form-control" name='f_name' value='<?php echo $f_name ?>' />
+        </div>
 
+        <div class="form-group">
+            <label for="name">middle name</label>
+            <input type="text"  name='m_name' value='<?php echo $m_name ?>' class="form-control" />
+        </div>
 
+        <div class="form-group">
+            <label for="name">Last name</label>
+            <input type="text" value='<?php echo $l_name ?>' name='l_name' class="form-control" />
+        </div>
 
+        <div class="form-group">
+            <label for="name">username</label>
+            <input type="text" value='<?php echo $username ?>'  name='username' class="form-control" />
+        </div>
+        <div class="form-group">
+            <label for="name">role</label>
+            <input type="text" value='<?php echo $rol?>' name='role' class="form-control" />
+        </div>
 
+        <div class="form-group">
+            <label for="name">Phone number</label>
+            <input type="text" value='<?php echo $phone_number?>'  name='phone_number' class="form-control" />
+        </div>
 
-<?php  include("./parts/footer.php") ?>
+        <div class="form-group">
+            <label for="name">photo</label>
+            <td> <input type="file" name="image"> </td>
+        </div>
+
+        <div class="form-group">
+            <label for="Email">Email</label>
+            <input type="email" value='<?php echo $email ?>' name='email' class="form-control" />
+        </div>
+
+            <?php }}?>
+
+    <div class="modal-footer">
+        <label for=""><a href="users.php">Close</a></label>
+        <input type="submit"  class="btn btn-warning" name='update' value="submit">
+    </div>
+
+    </div>
+    </form>
+</div>
+</div>
+</div>
+<!--  Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<?php
+if(isset($_POST['update'])){
+    $username = $_POST['username'];
+    $role = $_POST['role'];
+    $f_name = $_POST['f_name'];
+    $m_name = $_POST["m_name"];
+    $l_name=$_POST['l_name'];
+    $phone_number = $_POST["phone_number"];
+    $email = $_POST['email'];
+
+    if(!isset($_FILES['image']['name'])){
+        $image_name = "";
+    }
+    else{
+        $image_name = $_FILES['image']['name']; 
+        $image_source = $_FILES['image']['tmp_name'];
+        $image_destination = "../images/users/".$image_name;
+        $uplode = move_uploaded_file($image_source,$image_destination);
+        if($uplode==FALSE){
+            $_SESSION["add"]="faile to upload image";
+            #header("Location:".HOMEURL."/admin/users.php");
+            die();
+        }
+     
+    }
+
+    $query ="UPDATE `cbtp`.`users` SET
+             f_name='$f_name', m_name='$m_name', 
+             l_name='$l_name', username='$username', 
+             phone_number='$phone_number', 
+             email='$email', role='$role' ";
+
+    $result = mysqli_query($conn,$query)or die(mysqli_error());
+    if($result == True){
+        $_SESSION["add"]=$username." sucessfully added";
+        # header("Location:".HOMEURL."admin/users.php");
+         
+     }else{
+         $_SESSION["add"]=$username." failed to added";
+        # header("Location:".HOMEURL."admin/update_user.php");
+     }
+     
+     }else{
+         echo "btn not  clicked";
+     }
+     
+     
+
+  include("./parts/footer.php") ?>
