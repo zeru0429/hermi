85b84fecaa3a601baff64254fa6278f38e023bc7
zeru0429 <mihiretutigistu@gmail.com>
2023-06-26 23:48:04 +0300

    sql ready

--- hermi.sql
+++ hermi.sql
@@ -1,7 +1,8 @@
+drop database cbtp;
 CREATE database if not EXISTS `cbtp`;
 use `cbtp`;
 CREATE TABLE if not exists `cbtp`.`users` (
-  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL AUTO_INCREMENT,
   `f_name` VARCHAR(45) NOT NULL,
   `m_name` VARCHAR(45) NOT NULL,
   `l_name` VARCHAR(45) NOT NULL,
@@ -10,21 +11,24 @@ CREATE TABLE if not exists `cbtp`.`users` (
   `phone_number` VARCHAR(15) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
   `role` VARCHAR(45) NOT NULL,
+  `image_url` varcharacter(255) not null,
   PRIMARY KEY (`user_id`));
 
 CREATE TABLE if not exists `cbtp`.`mother_table` (
-  `m_id` INT NOT NULL,
+  `m_id` INT NOT NULL auto_increment,
   `f_name` VARCHAR(45) NOT NULL,
   `m_name` VARCHAR(45) NOT NULL,
   `l_name` VARCHAR(45) NOT NULL,
   `bithdate` DATE NOT NULL,
-  `photo_url`  VARCHAR(255),
-  `blood_type` VARCHAR(5) NULL,
-  `m_phone` VARCHAR(15) NULL,
-  `zone` VARCHAR(45) NULL,
-  `wereda` VARCHAR(45) NULL,
-  `kebele` VARCHAR(45) NULL,
+  `photo_url`  VARCHAR(255) not null,
+  `blood_type` VARCHAR(5) not NULL,
+  `m_phone` VARCHAR(15) not NULL,
+  `zone` VARCHAR(45) not NULL,
+  `wereda` VARCHAR(45) not NULL,
+  `kebele` VARCHAR(45) not NULL,
   PRIMARY KEY (`m_id`));
+
+
 
 CREATE TABLE if not exists `cbtp`.`child_table` (
   `c_id` INT NOT NULL auto_increment,
@@ -33,7 +37,7 @@ CREATE TABLE if not exists `cbtp`.`child_table` (
   `m_name` VARCHAR(45) NOT NULL,
   `l_name` VARCHAR(45) NOT NULL,
   `bithdate` DATE not NULL,
-  `blood_type` varchar(10) not null  
+  `blood_type` varchar(10) not null ,
   PRIMARY KEY (`c_id`),
     FOREIGN KEY (`m_id`)
     REFERENCES `cbtp`.`mother_table` (`m_id`)
@@ -69,6 +73,16 @@ CREATE TABLE if not exists `cbtp`.`child_vaccine` (
     REFERENCES `cbtp`.`child_table` (`c_id`)
     ON DELETE CASCADE );
 
+CREATE TABLE if not exists cbtp.post (
+  post_id INT not null auto_increment,
+  tittle text not null,
+  catagory text not null,
+  description text not null,
+  date_of_post DATETIME not null,
+  PRIMARY KEY (post_id)
+);
+
+
 CREATE TABLE if not exists cbtp.post_img (
   post_img_id INT auto_increment not null,
   post_id INT not null,
@@ -77,11 +91,7 @@ CREATE TABLE if not exists cbtp.post_img (
   FOREIGN KEY (post_id) REFERENCES cbtp.post (post_id)
 );
 
-INSERT INTO users (`user_id`,`f_name`,`m_name`,	`l_name`,`password`,`phone_number`,`email`,`role`,`image_url`)
-			values('20', 'mihiretu', 'tigistu', 'hailegiorgis', 'zeru', '21232f297a57a5a743894a0e4a801fc3', '+251949229226', 'mihiretutigistu@gmail.com', 'admin', 'IMG_0724.jpg');
-INSERT INTO users (`user_id`, `f_name`, `m_name`, `l_name`, `username`, `password`, `phone_number`, `email`, `role`, `image_url`)
- values ('21', 'abem', 'tezera', 'moshago', 'abem', '81dc9bdb52d04dc20036dbd8313ed055', '+251949229226', 'mihiretutigistu@gmail.com', 'registrar', '20230509_093234.jpg');
-
-
-
-
+insert into cbtp.users(f_name, m_name, l_name, username, password, phone_number, email, role, image_url)
+			values('mihiretu', 'tigistu', 'hailegiorgis', 'zeru',
+				'21232f297a57a5a743894a0e4a801fc3', '+251949229226', 
+				'mihiretutigistu@gmail.com', 'admin', 'zeru.jpg');
