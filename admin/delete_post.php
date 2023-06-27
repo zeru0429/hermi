cad7dcc1e94e919ceca0a0f8996c1c28f776c164
zeru0429 <mihiretutigistu@gmail.com>
2023-06-27 06:43:40 +0300

    delete post

--- admin/delete_post.php
+++ admin/delete_post.php
@@ -3,12 +3,12 @@
 // find id
 $id = $_GET['id'];
 // delete from db
-$query = "DELETE FROM `cbtp`.`post` WHERE post_id ='$id'";
-$result = mysqli_query($conn,$query)or die(mysqli_error());
-if($result == True){
-   $query1 = "DELETE FROM `cbtp`.`post_img` WHERE post_id ='$id'";
-   $result1 = mysqli_query($conn,$query1)or die(mysqli_error()); 
-   if($result1 == True){
+$query1 = "DELETE FROM `cbtp`.`post_img` WHERE post_id ='$id'";
+$result1 = mysqli_query($conn,$query1)or die(mysqli_error()); 
+if($result1 == True){
+   $query = "DELETE FROM `cbtp`.`post` WHERE post_id ='$id'";
+   $result = mysqli_query($conn,$query)or die(mysqli_error());
+   if($result == True){
     $_SESSION["add"]=$id." deleted successfully";
      header("Location:".HOMEURL."admin/post.php");}
      else{
